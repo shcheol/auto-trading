5ba38b449b3c8cedfa1fd88b64511b2c6fc4ad0e
unknown <kian1523@naver.com>
2024-10-16 11:57:09 +0900

    init

---
+++ src/main/java/com/hcs/autotrading/AutoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.hcs.autotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcs/autotrading/AutoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcs.autotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
