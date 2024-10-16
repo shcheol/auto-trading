2645590f92a834d35239ffe70890919491236b6c
unknown <kian1523@naver.com>
2024-10-16 18:26:53 +0900

    setting

---
+++ src/main/java/com/hcs/autotrading/AutoTradingService.java
@@ -0,0 +1,18 @@
+package com.hcs.autotrading;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutoTradingService {
+
+	public void doAutoTrade(){
+
+		// 1. getchartdata
+
+		// 2. buy/sell/hold 판단
+
+		// 3. 실제 자동 매매 진행
+
+		// 4. slack bot 알림
+	}
+}
